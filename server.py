f6d7d803a8d21d4a6111e96ffc60b4f29412a01c
bmilkman <78684039+bmilkman@users.noreply.github.com>
2024-04-10 16:54:00 +0800

    Update server.py

--- server.py
+++ server.py
@@ -4,12 +4,14 @@ from starlette.responses import JSONResponse
 import httpx
 import asyncio
 import uuid
+import torch
 
 app = FastAPI()
-SUMMARIZE_APIS = 6
+# 假设有n张卡，用n-1张卡去异步处理大流量的请求，用1张卡同步慢慢处理流量小的请求
+SUMMARIZE_APIS = torch.cuda.device_count() - 1 
 FEEDBACK_PORT = SUMMARIZE_APIS
 
-
+# 这里是我的样例request，仅作参考
 class SummarizeRequest(BaseModel):
     text: str
     timestamp: str
@@ -24,13 +26,13 @@ class FeedbackRequest(BaseModel):
     text: str
     
 results = {}
-# Creating an asyncio queue
+# 使用queue的方式去囤积请求
 task_queue = asyncio.Queue()
 
 async def worker(api_url):
     async with httpx.AsyncClient() as client:
         while True:
-            # Wait for an item from the queue
+            # 每个worker去queue里拿任务
             task_id, item = await task_queue.get()
             try:
                 response = await client.post(api_url, json=item.dict())
@@ -39,12 +41,14 @@ async def worker(api_url):
                 results[task_id] = {'status': 'error', 'data' :str(e)}
             finally:  
                 #print("Processed:", response)
+                # 这是个很奇怪的说法，在异步的queue里需要通过这个来确认get的东西没了
                 task_queue.task_done()
 
 @app.on_event("startup")
 async def startup_event():
+    # 假设你的LLM API的地址和端口是这样的
     api_urls = [f"http://0.0.0.0:500{i}/summarize" for i in range(SUMMARIZE_APIS)]
-    # Create multiple worker tasks to process the queue concurrently
+    # 一个API分配一个worker
     for api_url in api_urls:
         asyncio.create_task(worker(api_url))
         print(f"Workers started for {api_url}")
@@ -55,16 +59,17 @@ async def submit_data(item: SummarizeRequest):
     await task_queue.put((task_id,item))
     return JSONResponse(content={"message": "Data added to queue", 'task_id' : task_id})
 
+# 这里把异步操作的分发和收集分开了，适用于每天需要对后台数据进行大量更新的任务。
 @app.get("/results/{task_id}")
 async def get_result(task_id: str):
-    result = results.get(task_id)
+    result = results.pop(task_id, None)
     if result and result["status"] == "completed":
         return result["data"]
     elif result and result["status"] == "error":
         raise HTTPException(status_code=500, detail=result["data"])
     else:
         raise HTTPException(status_code=404, detail="Result not available or task not found")
-        
+# 同步   
 @app.post("/feedback")
 async def submit_feedback(feedback: FeedbackRequest):
     try:
